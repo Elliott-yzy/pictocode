44c4728a2fb5afd209c5084cc55bfcf9f08c80d2
杨赵艺 <yangzhaoyi@YZYMacBook-Pro.local>
2024-09-25 21:21:44 +1000

    create script.js

---
+++ task3/script.js
@@ -0,0 +1,14 @@
+function updateHeading() {
+  const rewardHeading = document.querySelector('.rewards-box h2');
+  const rewardText = document.querySelector('.rewardsbox-text');
+  if (window.innerWidth <= 356) {
+    rewardHeading.textContent = 'COMP6080 Customer Rewards';
+    rewardText.textContent = 'Save on GitHub Pro, Nitro more';
+  } else {
+    rewardHeading.textContent = 'COMP6080 Rewards';
+    rewardText.textContent = 'Save on GitHub Pro, Nitro and more';
+  }
+}
+
+window.addEventListener('load', updateHeading);
+window.addEventListener('resize', updateHeading);
